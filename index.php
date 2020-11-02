b4d6ed82eb53b2bd4f045210c7f175460b199660
dishamehta1995 <43091075+dishamehta1995@users.noreply.github.com>
2020-11-02 16:10:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,26 @@
 <html>
 <head>
 	<title>Characther remover</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0,user-scalable=0">
 </head>
 <style type="text/css">
-	body{color: #fff;background: #111;font-size: 18px; line-height: 1.5;font-family: arial;}
+	body{color: #fff;background: #111;font-size: 18px; line-height: 1.5;font-family: arial;box-sizing: border-box;}
 	p{text-transform: lowercase;}
+	button{font-size: 16px; padding: 10px 20px; border-radius: 25px;}
 	textarea {width: 100%; min-height: 100px; }
+
 </style>
 <body>
 	<textarea></textarea>
-	<button>Submit</button>
+	<button class="button">Submit</button>
+	<input type="text" value="Hello World" id="myInput">
+	<button onclick="myFunction()">Copy text</button>
 	<p>	</p>
 </body>
 <script type="text/javascript" src="jquery.min.js"></script>
 <script>
 $(function (){
-	$('button').on('click',function(){
+	$('.button').on('click',function(){
 	  var str = $("textarea").val(); 
 	  var newStr = str.replace(new RegExp('[,.’?!”“:;:@#$%^&*()+={}|"/><~`]', 'g'), '');
 	  var newStr = newStr.replace(new RegExp('[-—]', 'g'), ' ');
